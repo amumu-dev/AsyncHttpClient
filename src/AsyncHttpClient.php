e30491a0d6b16dfa2dc5cc08f4fba545419215d4
yancan <yancan@yongche-inc.com>
2011-11-15 16:27:39 +0800

    add headers & body

--- src/AsyncHttpClient.php
+++ src/AsyncHttpClient.php
@@ -29,7 +29,6 @@ class AsyncHttpClient {
     const ENC_FORMDATA   = 'multipart/form-data';
 
     protected $uri = null;
-    protected $uriInfo = array();
     protected $headers = array();
     protected $method = self::GET;
     protected $paramsGet = array();
@@ -38,6 +37,7 @@ class AsyncHttpClient {
     protected $timeout = 3;
     protected $response = null;
     protected $callback = null;
+    protected $files = array();
 
     public $config = array(
         'eventbase' => null,
@@ -73,7 +73,6 @@ class AsyncHttpClient {
 
     public function setUri($uri) {
         $this->uri = $uri;
-        $this->uriInfo = parse_url($this->uri);
         return $this;
     }
 
@@ -100,6 +99,14 @@ class AsyncHttpClient {
         }
 
         return $this;
+    }
+
+    public function setHeader($key, $value) {
+        $this->headers[$key] = $value;
+    }
+
+    public function setHeaders(array $headers) {
+        $this->headers = $headers;
     }
 
     public function setParameterPost($name, $value = null) {
@@ -133,7 +140,6 @@ class AsyncHttpClient {
     }
 
     public function request($callback = null) {
-        $tmp = $this->uriInfo;
         $port = isset($tmp['port']) ? $tmp['port'] : 80;
         $socket = stream_socket_client("$tmp[host]:$port", $errno, $errstr, 
             $this->timeout, STREAM_CLIENT_ASYNC_CONNECT | STREAM_CLIENT_CONNECT); 
@@ -158,7 +164,9 @@ class AsyncHttpClient {
     }
 
     public function onAccept($socket, $event, $args) {
-        $tmp = $this->uriInfo;
+        $body = $this->_prepareBody();
+        $headers = $this->_prepareHeaders();
+
         $url = isset($tmp['query']) ? "$tmp[path]?$tmp[query]" : $tmp['path'];
         $out = "$this->method $url HTTP/1.1\r\n";
         $out .= "Host: $tmp[host]\r\n";
@@ -182,8 +190,144 @@ class AsyncHttpClient {
         }
     }
 
-    protected function _prepareBody() {}
+    protected function _prepareBody() {
+        $mbIntEnc = mb_internal_encoding();
+        mb_internal_encoding('ASCII');
+        $body = '';
 
-    protected function _prepareHeaders() {}
+        // If we have files to upload, force enctype to multipart/form-data
+        if (count ($this->files) > 0) {
+            $this->setEncType(self::ENC_FORMDATA);
+        }
+
+        // If we have POST parameters or files, encode and add them to the body
+        if (count($this->paramsPost) > 0 || count($this->files) > 0) {
+            switch($this->enctype) {
+                case self::ENC_FORMDATA:
+                    // Encode body as multipart/form-data
+                    $boundary = '---ASYNCHTTPCLIENT-' . md5(microtime());
+                    $this->setHeader(self::CONTENT_TYPE, self::ENC_FORMDATA . "; boundary={$boundary}");
+
+                    // Get POST parameters and encode them
+                    $params = self::_flattenParametersArray($this->paramsPost);
+                    foreach ($params as $pp) {
+                        $body .= self::encodeFormData($boundary, $pp[0], $pp[1]);
+                    }
+
+                    // Encode files
+                    foreach ($this->files as $file) {
+                        $fhead = array(self::CONTENT_TYPE => $file['ctype']);
+                        $body .= self::encodeFormData($boundary, $file['formname'], $file['data'], $file['filename'], $fhead);
+                    }
+
+                    $body .= "--{$boundary}--\r\n";
+                    break;
+
+                case self::ENC_URLENCODED:
+                    // Encode body as application/x-www-form-urlencoded
+                    $this->setHeader(self::CONTENT_TYPE, self::ENC_URLENCODED);
+                    $body = http_build_query($this->paramsPost, '', '&');
+                    break;
+
+                default:
+                    throw new InvalidArgumentException("Cannot handle content type '{$this->enctype}' automatically." .
+                    break;
+            }
+        }
+
+        // Set the Content-Length if we have a body or if request is POST/PUT
+        if ($body || $this->method == self::POST || $this->method == self::PUT) {
+            $this->setHeader(self::CONTENT_LENGTH, strlen($body));
+        }
+
+        if (isset($mbIntEnc)) {
+            mb_internal_encoding($mbIntEnc);
+        }
+
+        return $body;
+    }
+
+    protected function _prepareHeaders() {
+        $headers = array();
+        $uriInfo = parse_url($this->uri);
+        $uriInfo['port'] = isset($uriInfo['port']) ? 
+            $uriInfo['port'] : ($uriInfo['scheme'] == 'https' ? 443 : 80);
+        $host = $uriInfo['host'];
+
+        // If the port is not default, add it
+        if (!(($uriInfo['scheme'] == 'http' && $uriInfo['port'] == 80) ||
+              ($uriInfo['scheme'] == 'https' && $uriInfo['port'] == 443))) {
+            $host .= ':' . $uriInfo['port'];
+        }
+        $headers[] = "Host: {$host}";
+        $headers[] = "Connection: close";
+        $headers[] = function_exists('gzinflate') ? 
+            'Accept-encoding: gzip, deflate' : 'Accept-encoding: identity';
+
+        if ($this->method == self::POST && isset($this->enctype)) {
+            $headers[] = self::CONTENT_TYPE . ': ' . $this->enctype;
+        }
+
+        foreach ($this->headers as $header) {
+            list($name, $value) = $header;
+            if (is_array($value)) {
+                $value = implode(', ', $value);
+            }
+
+            $headers[] = "$name: $value";
+        }
+
+        return $headers;
+    }
+
+    static protected function _flattenParametersArray($parray, $prefix = null) {
+        if (! is_array($parray)) {
+            return $parray;
+        }
+
+        $parameters = array();
+
+        foreach($parray as $name => $value) {
+
+            // Calculate array key
+            if ($prefix) {
+                if (is_int($name)) {
+                    $key = $prefix . '[]';
+                } else {
+                    $key = $prefix . "[$name]";
+                }
+            } else {
+                $key = $name;
+            }
+
+            if (is_array($value)) {
+                $parameters = array_merge($parameters, self::_flattenParametersArray($value, $key));
+
+            } else {
+                $parameters[] = array($key, $value);
+            }
+        }
+
+        return $parameters;
+    }
+
+    public static function encodeFormData($boundary, $name, $value, $filename = null, $headers = array()) {
+        $ret = "--{$boundary}\r\n" .
+            'Content-Disposition: form-data; name="' . $name .'"';
+
+        if ($filename) {
+            $ret .= '; filename="' . $filename . '"';
+        }
+        $ret .= "\r\n";
+
+        foreach ($headers as $hname => $hvalue) {
+            $ret .= "{$hname}: {$hvalue}\r\n";
+        }
+        $ret .= "\r\n";
+
+        $ret .= "{$value}\r\n";
+
+        return $ret;
+    }
 
 }
